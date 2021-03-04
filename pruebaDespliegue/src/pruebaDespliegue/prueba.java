59ae229c656ae8d82580318ef528d814cd194e9e
gigif <gigif@Vengeance.mshome.net>
2021-03-04 20:50:51 +0100

    segundo commit

--- pruebaDespliegue/src/pruebaDespliegue/prueba.java
+++ pruebaDespliegue/src/pruebaDespliegue/prueba.java
@@ -3,5 +3,6 @@ package pruebaDespliegue;
 public class prueba {
 	public static void main(String[] args) {
 	System.out.println("Primera prueba de repositorio");
+	System.out.println("segundo commit");
 }
 }
